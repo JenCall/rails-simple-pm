58364379ee16fa4178b106aa133d7972da49a8f8
Jeniffer Callou <jenifferprc@gmail.com>
2021-11-17 15:45:46 +0100

    configured the show page + redirect pages

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,11 @@
 class Task < ApplicationRecord
   belongs_to :project
+
+  validates :status, inclusion: { in: ['not-started', 'in-progress', 'done'] }
+
+  STATUS_OPTIONS = [
+    ['Not started','not-started'],
+    ['In progress','in-progress'],
+    ['Done','done']
+  ]
 end
