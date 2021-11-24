e532adfc1f7dc74b5682f6b521634c5d73d1cbc4
Jeniffer Callou <jenifferprc@gmail.com>
2021-11-24 21:46:58 +0100

    add list items + corrected  app js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("sortable")
+
+import Sortable from 'sortablejs';
 
 import "../stylesheets/application"
 
@@ -17,3 +18,7 @@ import "../stylesheets/application"
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+document.addEventListener('turbolinks:load', () => {
+  console.log("Sortable", Sortable)
+})
